1871ebb478bbb8be291b10ca8a47abaaa27dc0db
ccmeyers-dev <charlotteccmeyers@outlook.com>
2021-08-07 17:58:53 +0100

    removed test mailhog transporter

--- functions/server/configs/mail.js
+++ functions/server/configs/mail.js
@@ -25,11 +25,7 @@ const advancedMode =
   process.env.REACT_APP_ADVANCED_MAILER &&
   process.env.REACT_APP_ADVANCED_MAILER.toLowerCase() === "true";
 
-// const transporter = advancedMode ? advancedTransporter : gmailTransporter;
-const transporter = nodemailer.createTransport({
-  host: "localhost",
-  port: 1025,
-});
+const transporter = advancedMode ? advancedTransporter : gmailTransporter;
 
 const template = (file, context) => {
   const source = fs.readFileSync(
