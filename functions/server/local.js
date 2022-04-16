bd6a68833047207ac4ad894187f2fe97108cd92b
Charlotte Meyers <charlotteccmeyers@outlook.com>
2022-04-16 12:44:21 -0700

    added proxy

--- functions/server/local.js
+++ functions/server/local.js
@@ -4,7 +4,7 @@ require("dotenv").config({
 
 const app = require("./app");
 
-const PORT = process.env.PORT || 3000;
+const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
